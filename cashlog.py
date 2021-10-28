11c76f06fa9a0b139990b0020fc162233892569e
Alec <alec.hampton@charter.com>
2021-10-28 15:29:15 -0600

    Added dev wallet csv file; a little progress on main script

--- cashlog.py
+++ cashlog.py
@@ -1,4 +1,7 @@
 
+import numpy as np
+import pandas as pd
+
 EXIT_CODES = ('X', 'x', 'exit', 'back')
 ACTION_DIVIDER = "\n\n\n\n\n\n-----------------------------------\n\n"
 
@@ -7,6 +10,7 @@ def prompt_enter_txn():
     date = None
     while True:
         print(ACTION_DIVIDER + "Enter Transaction Details:\nEnter 'X' for any field when finished entering transactions.\n")
+        # TODO: Use a dictionary/map for the number-type relationship here
         type = input("Type (1. Debit 2. Credit): ")
         if type in EXIT_CODES:
             break
@@ -15,8 +19,14 @@ def prompt_enter_txn():
             break
 
 def main():
+    wallet = pd.read_csv("wallet.csv")
+    print(wallet)
+    for index, item in wallet.iterrows():
+        print(item['envelope'] + '\t\t\t\t' + str(item['balance']))
+
     action = ""
     while action not in EXIT_CODES:
+        # TODO: Use an actual map/dictionary object to define which numbers correspond to which actions, and automate the generation of this string based on the dictionary
         print(ACTION_DIVIDER + "Available Tasks:\n1. Enter a transaction\n2. Allocate funds based on a pre-configured distribution\n3. Transfer funds between envelopes\nX. Exit\n")
         action = input("Choose a task: ")
         match action:
